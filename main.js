1e5c6c935e6d1e37b9ff2279a74a7b988556014f
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-07-26 13:41:02 -0700

    Set up new canvas

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.getElementById(canvas),
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
@@ -26,3 +28,20 @@ function animate() {
 
   renderer.render(scene, camera);
 }
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  jeff.rotation.y += 0.01;
+  jeff.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
