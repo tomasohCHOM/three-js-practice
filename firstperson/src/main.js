bb5bbde8b4f2b174e5af61f8b296a3a68cb6e58b
Tomas Oh <tomasoh@csu.fullerton.edu>
2026-01-27 13:18:50 -0800

    adapt simple example into class

--- firstperson/src/main.js
+++ firstperson/src/main.js
@@ -1,41 +1,62 @@
 import "./style.css";
 import * as THREE from "three";
+// import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
+// import { FirstPersonControls } from "three/examples/jsm/Addons.js";
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color("#f0f0f0");
+class FirstPersonDemo {
+  constructor() {
+    this._renderer = new THREE.WebGLRenderer();
+    this._renderer.setPixelRatio(window.devicePixelRatio);
+    this._renderer.setSize(window.innerWidth, window.innerHeight);
 
-const aspect = window.innerWidth / window.innerHeight;
-const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 100);
-camera.position.z = 5;
+    // Scene
+    this._scene = new THREE.Scene();
+    this._scene.background = new THREE.Color("#fff");
 
-const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
-const cubeMaterial = new THREE.MeshStandardMaterial({
-  color: "#468585",
-  emissive: "#468585",
-});
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(cube);
+    // Camera
+    const fov = 75;
+    const aspect = window.innerWidth / window.innerHeight;
+    const near = 1.0;
+    const far = 100;
+    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this._camera.position.z = 5;
 
-const light = new THREE.DirectionalLight(0xffffff, 20);
-light.position.set(1, 1, 1);
-scene.add(light);
+    // Light
+    let light = new THREE.AmbientLight(0xffffff, 10);
+    this._scene.add(light);
+    light = new THREE.DirectionalLight(0xffffff, 5);
+    light.position.set(1, 1, 1);
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.getElementById("canvas"),
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
+    this._scene.add(light);
 
-window.addEventListener("resize", () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-});
+    // Cube
+    const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+    const cubeMaterial = new THREE.MeshStandardMaterial({ color: "#468585" });
+    this._cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    this._scene.add(this._cube);
 
-function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+    document.body.appendChild(this._renderer.domElement);
+    window.addEventListener("resize", () => this._onWindowResize());
+
+    this._RAF();
+  }
+
+  _onWindowResize() {
+    this._camera.aspect = window.innerWidth / window.innerHeight;
+    this._camera.updateProjectionMatrix();
+    this._renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  _RAF() {
+    requestAnimationFrame(() => {
+      this._cube.rotation.x += 0.01;
+      this._cube.rotation.y += 0.01;
+      this._renderer.render(this._scene, this._camera);
+      this._RAF();
+    });
+  }
 }
 
-animate();
+window.addEventListener("DOMContentLoaded", () => {
+  new FirstPersonDemo();
+});
